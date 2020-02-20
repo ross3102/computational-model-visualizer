77815717af79103902df8dfed415af09ddf898c2
sebwit20 <sebwit20@bergen.org>
2020-02-20 12:57:19 -0500

    test case modals and stuff

--- main.php
+++ main.php
@@ -1,11 +1,10 @@
 <?php
 
 require_once dirname(__FILE__) . "/config.php";
-require_once dirname(__FILE__). "/vendor/autoload.php";
 
 require_once dirname(__FILE__) . "/model/machine_db.php";
 
-$user_id = 21;
+$user_id = 31;
 $user = get_user_by_id($user_id);
 
 function generateHeader($head="") {
@@ -24,17 +23,19 @@ function generateHeader($head="") {
             <!--Let browser know website is optimized for mobile-->
             <meta name='viewport' content='width=device-width, initial-scale=1.0'/>
         " . $head . " </head>
-        <body class=''>
-            <nav class='blue'>
+        
+        
+        <body class='teal accent-1'>
+            <nav class='pink accent-3'>
             <div class='nav-wrapper' style='padding: 0 30px;'>
-                <a href='/" . $web_root . "' class='brand-logo'>CMV</a>
+                <a href='#' class='brand-logo'>CMV</a>
                 <ul id='nav-mobile' class='right hide-on-med-and-down'>
                     <li><a href='#'>Hello, " . $user["username"] . "</a></li>
                 </ul>
             </div>
-            </nav>
+            </nav>        
             <main>
-                <div class='container z-depth-3 white' id='main-box' style='padding: 10px'>
+                <div class='container z-depth-3 white' id='main-box'>
           
     ";
 }
--- room_view/view.php
+++ room_view/view.php
@@ -8,7 +8,7 @@ generateHeader(""); ?>
         <h5 style="text-decoration: underline;">Questions</h5>
         <ul class="collection">
             <?php foreach ($questions as $question) { ?>
-                <li class="collection-item"><?php echo $question["text"] ?><span class="badge"><a href="./index.php?action=delete_question&question_id=<?php echo $question["question_id"] ?>&room_id=<?php echo $room_id ?>"><i class="material-icons red-text">delete</i></a></span></li>
+                <li class="collection-item"><?php echo $question["text"] ?><span class="badge"><a href="#displaytest" class="modal-trigger"><i class="material-icons black-text">pregnant_woman</i></a><a onclick=write_test(<?php echo $question["question_id"]?>)><i class="material-icons blue-text">assignment</i></a><a href="./index.php?action=delete_question&question_id=<?php echo $question["question_id"] ?>&room_id=<?php echo $room_id ?>"><i class="material-icons red-text">delete</i></a></span></li>
             <?php } ?>
         </ul>
         <div class="center-align">
@@ -16,7 +16,38 @@ generateHeader(""); ?>
             <br>
             <br>
             <a href="./index.php?action=create&room_id=<?php echo $room_id ?>" class="btn btn-large waves-effect waves-light blue lighten-1"><?php echo $room["room_code"] == null ? "Open Room": "View Room" ?></a>
-
+            <div class="modal" id="writetests">
+                <div class="modal-content">
+                    <h4 class="modal-header">Write a Test Case!</h4>
+                        <form action="./index.php" method="post" id="addtest">
+                            <input type="hidden" name="action" value="add_case">
+                            <input type="hidden" name="question_id" id="question_id">
+                            <input type="hidden" name="room_id" value="<?php echo $room_id ?>">
+                            <div class="input-field">
+                                <textarea class="materialize-textarea" name="test_case" id="test_case"></textarea>
+                                <label for="test_case">Test Case</label>
+                            </div>
+                            <div class="input-field">
+                                <input type="text" name="fail" id="fail">
+                                <label for="fail">Pass or Fail (P/F)</label>
+                            </div>
+                        </form>
+                </div>
+                <div class="modal-footer">
+                    <a class="modal-close waves-effect waves-light btn-flat green-text" onclick="$('#addtest').submit()">Submit</a>
+                    <a class="modal-close waves-effect waves-green btn-flat red-text">Close</a>
+                </div>
+            </div>
+            <div class="modal" id="displaytest">
+                <div class="modal-container">
+                    <h4 class="modal-header">Test Cases</h4>
+                    <ul class="collection">
+                        <?php foreach ($questions as $question) {?>
+                        <li class="collection-item"></li>
+                        <?php } ?>
+                    </ul>
+                </div>
+            </div>
             <div class="modal" id="add-questionm">
                 <div class="modal-content">
                     <h4 class="modal-header">Add Question</h4>
@@ -39,5 +70,11 @@ generateHeader(""); ?>
     </div>
     </div>
 </div>
-
+<script>
+    function write_test(question_id) {
+        $("#writetests #question_id").val(question_id);
+        $("#writetests").modal("open");
+    }
+</script>
 <?php generateFooter() ?>
+
