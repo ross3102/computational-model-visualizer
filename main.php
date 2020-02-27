e942e3e419089f4faf090d376f7212662481dccb
Andrew Kum <andrew3840@gmail.com>
2020-02-27 13:27:02 -0500

    Merge branch 'master' of C:\xampp\htdocs\computational-model-visualizer with conflicts.

--- main.php
+++ main.php
@@ -1,10 +1,11 @@
 <?php
 
 require_once dirname(__FILE__) . "/config.php";
+require_once dirname(__FILE__). "/vendor/autoload.php";
 
 require_once dirname(__FILE__) . "/model/machine_db.php";
 
-$user_id = 31;
+$user_id = 41;
 $user = get_user_by_id($user_id);
 
 function generateHeader($head="") {
@@ -23,19 +24,17 @@ function generateHeader($head="") {
             <!--Let browser know website is optimized for mobile-->
             <meta name='viewport' content='width=device-width, initial-scale=1.0'/>
         " . $head . " </head>
-        
-        
-        <body class='teal accent-1'>
-            <nav class='pink accent-3'>
+        <body class=''>
+            <nav class='blue'>
             <div class='nav-wrapper' style='padding: 0 30px;'>
-                <a href='#' class='brand-logo'>CMV</a>
+                <a href='/" . $web_root . "' class='brand-logo'>CMV</a>
                 <ul id='nav-mobile' class='right hide-on-med-and-down'>
                     <li><a href='#'>Hello, " . $user["username"] . "</a></li>
                 </ul>
             </div>
-            </nav>        
+            </nav>
             <main>
-                <div class='container z-depth-3 white' id='main-box'>
+                <div class='container z-depth-3 white' id='main-box' style='padding: 10px'>
           
     ";
 }
